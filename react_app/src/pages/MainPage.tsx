ed9c2a7ec1b10daa0b1b4f19d906dd6fafc06d72
Benjamin Copen <pinj2000@gmail.com>
2023-09-06 13:04:59 -0400

    additional info on front page

--- react_app/src/pages/MainPage.tsx
+++ react_app/src/pages/MainPage.tsx
@@ -14,6 +14,7 @@ export default function MainPage(){
             <div className="main-page-headers">
                 <h1 className="main-page-h1" >rMEND: your recommendations are here</h1>
                 <h4 className="main-page-h4">Find recommendations from all over for various forms of medias</h4>
+                <h4 className="main-page-h4">Recommend medias for other forms of media such as Books, Movies, Shows, Albums, Games, and Podcasts</h4>
                 <h5>ie. search Breaking Bad and recommend Better Call Saul..</h5>
                 <h6>It's all here</h6>
             </div>
